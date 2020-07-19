e1d48dc2fc184d516d0e72add5141302194eadc1
Nguyễn Văn Hữu <58680242+LongHuu100@users.noreply.github.com>
2020-07-19 09:05:45 +0700

    Create AnalogToDigitAdapter.java

---
+++ AnalogToDigitAdapter.java
@@ -0,0 +1,75 @@
+package eric.designpattern.adapter;
+
+import java.util.Arrays;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * <p>
+ * Adapter - convert analog data to digit data.
+ * </p>
+ * 
+ * @author eric
+ * @date Mar 8, 2016 1:07:00 PM
+ */
+public class AnalogToDigitAdapter implements DigitSignal {
+    public static final float DEFAULT_THRESHOLD_FLOAT_TO_BIN = 1.0f; // default threshold,
+    private Logger logger = LoggerFactory.getLogger(this.getClass());
+
+    private AnalogSignal analogSignal;
+    private byte[] digitData;
+    private float threshold;
+    private boolean cached;
+
+    public AnalogToDigitAdapter(AnalogSignal analogSignal) {
+        this(analogSignal, DEFAULT_THRESHOLD_FLOAT_TO_BIN);
+    }
+
+    public AnalogToDigitAdapter(AnalogSignal analogSignal, float threshold) {
+        this.analogSignal = analogSignal;
+        this.threshold = threshold;
+        this.cached = false;
+    }
+
+    @Override
+    public synchronized byte[] getDigit() {
+        if (!cached) {
+            float[] analogData = analogSignal.getAnalog();
+            int len = analogData.length;
+            digitData = new byte[len];
+
+            for (int i = 0; i < len; i++) {
+                digitData[i] = floatToByte(analogData[i]);
+            }
+        }
+
+        return digitData;
+    }
+
+    // not supported, should set the inner analog data instead,
+    @Override
+    public void setDigit(byte[] digitData) {
+        throw new UnsupportedOperationException();
+    }
+
+    public synchronized void setAnalogData(float[] analogData) {
+        invalidCache();
+        this.analogSignal.setAnalog(analogData);
+    }
+
+    public synchronized void invalidCache() {
+        cached = false;
+        digitData = null;
+    }
+
+    @Override
+    public void printDigit() {
+        logger.info("{}", Arrays.toString(getDigit()));
+    }
+
+    // float -> byte convert,
+    private byte floatToByte(float f) {
+        return (byte) (f >= threshold ? 1 : 0);
+    }
+}
